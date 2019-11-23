8fa0cd54faadd03ea3057b715ec3d9fa49329154
Frank Ford <ford.thecreativedev@gmail.com>
2019-11-23 01:22:25 -0500

    Added: httpclient services

--- src/ngx-httpclient-starter/src/app/projects/project-list/project-list.component.ts
+++ src/ngx-httpclient-starter/src/app/projects/project-list/project-list.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ProjectDataService } from "../services/project-data.service";
+import { Project } from "../models/project";
 
 @Component({
-  selector: 'app-project-list',
-  templateUrl: './project-list.component.html',
-  styleUrls: ['./project-list.component.scss']
+  selector: "app-project-list",
+  templateUrl: "./project-list.component.html",
+  styleUrls: ["./project-list.component.scss"]
 })
 export class ProjectListComponent implements OnInit {
+  projects: Project[];
 
-  constructor() { }
+  constructor(private ds: ProjectDataService) {}
 
   ngOnInit() {
+    this.projects = this.ds.list();
   }
-
 }
--- src/ngx-httpclient-starter/src/app/projects/services/project-data.service.ts
+++ src/ngx-httpclient-starter/src/app/projects/services/project-data.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Project } from "../models/project";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ProjectDataService {
+  projects: Project[];
+  project: Project;
 
-  constructor() { }
+  constructor(private httpClient: HttpClientModule) {}
+
+  list() {
+    return this.projects;
+  }
+
+  details(id: number) {
+    return this.project;
+  }
+
+  edit(id: number) {
+    return this.project;
+  }
 }
