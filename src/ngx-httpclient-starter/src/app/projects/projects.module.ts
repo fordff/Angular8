43d275feb51ad14e8230fdfc525e155eca9c3d25
fordff_dev <fordff7293@gmail.com>
2019-11-23 13:57:46 -0500

    Added prettierrc configuartion

--- src/ngx-httpclient-starter/src/app/projects/projects.module.ts
+++ src/ngx-httpclient-starter/src/app/projects/projects.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { ProjectsRoutingModule } from "./projects-routing.module";
-import { ProjectsComponent } from "./projects.component";
-import { QuestionsComponent } from "./questions/questions.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { NotesComponent } from "./notes/notes.component";
-import { HistoryComponent } from "./history/history.component";
-import { HttpClientModule } from "@angular/common/http";
-import { FormsModule } from "@angular/forms";
-import { ReactiveFormsModule } from "@angular/forms";
-import { ProjectNewComponent } from "./project-new/project-new.component";
-import { ProjectListComponent } from "./project-list/project-list.component";
-import { ProjectDetailsComponent } from "./project-details/project-details.component";
-import { ProjectEditComponent } from "./project-edit/project-edit.component";
+import { ProjectsRoutingModule } from './projects-routing.module';
+import { ProjectsComponent } from './projects.component';
+import { QuestionsComponent } from './questions/questions.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { NotesComponent } from './notes/notes.component';
+import { HistoryComponent } from './history/history.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ProjectNewComponent } from './project-new/project-new.component';
+import { ProjectListComponent } from './project-list/project-list.component';
+import { ProjectDetailsComponent } from './project-details/project-details.component';
+import { ProjectEditComponent } from './project-edit/project-edit.component';
 
 @NgModule({
   declarations: [
@@ -25,14 +25,14 @@ import { ProjectEditComponent } from "./project-edit/project-edit.component";
     ProjectNewComponent,
     ProjectListComponent,
     ProjectDetailsComponent,
-    ProjectEditComponent
+    ProjectEditComponent,
   ],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    ProjectsRoutingModule
-  ]
+    ProjectsRoutingModule,
+  ],
 })
 export class ProjectsModule {}
